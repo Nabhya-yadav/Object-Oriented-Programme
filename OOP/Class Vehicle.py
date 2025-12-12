9725c859b81d4cd53542a1f4adda5148db40824c
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-12-12 12:54:57 +0530

    Add files via upload

---
+++ OOP/Class Vehicle.py
@@ -0,0 +1,9 @@
+class vehicle: 
+    def __init__ (self,speed,mileage):
+        self.speed= speed
+        self.mileage=mileage
+
+modelX=vehicle(240,15)
+
+print(modelX.speed)
+print(modelX.mileage)
---
+++ OOP/Oop.py
@@ -0,0 +1,11 @@
+
+class motorbikes():
+    def Kawasaki(self):
+        print("Kawasaki ninja")
+
+    def Suzuki(self):
+        print("Suzuki hyabusa")
+
+object=motorbikes()
+object.Kawasaki()
+object.Suzuki()
